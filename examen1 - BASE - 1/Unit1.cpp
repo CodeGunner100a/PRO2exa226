8bea51f89441318ff3f114241396171212caae2d
CodeGunner100a <codegunner178@gmail.com>
2026-09-19 08:51:23 -0400

    Update Unit1.cpp

--- examen1 - BASE - 1/Unit1.cpp
+++ examen1 - BASE - 1/Unit1.cpp
@@ -131,34 +131,4 @@ void __fastcall TForm1::Button3Click(TObject *Sender) {
 
 // EJERCICIO 3
 
-void CargarDonut(TStringGrid *m, byte f, byte c, byte nf, byte nc, byte d) {
-	if (f < nf) // caso general
-	{
-		if (c < nc) {
-			if (f == 0 || f == nf - 1 || c == 0 || c == nc - 1) {
 
-				m->Cells[c][f] = d;
-			}
-			else {
-				m->Cells[c][f] = 0;
-			}
-
-			CargarDonut(m, f, c + 1, nf, nc, d);
-		}
-		else {
-			CargarDonut(m, f + 1, 0, nf, nc, d);
-		}
-	}
-}
-
-
-void __fastcall TForm1::Button4Click(TObject *Sender) {
-	byte m = StrToInt(Edit1->Text);
-	byte n = StrToInt(Edit2->Text);
-	byte d = StrToInt(Edit3->Text);
-
-	StringGrid1->RowCount = m;
-	StringGrid1->ColCount = n;
-
-	CargarDonut(StringGrid1, 0, 0, m, n, d);
-}
